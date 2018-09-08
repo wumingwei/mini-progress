7822a97c974800fc1f22d3427cd1b5d6b388f087
wumingwei <wu_mingwei@yeah.net>
2018-09-08 13:10:32 +0800

    add  depoendencies: wafer2-client-sdk

--- server/controllers/menulist.js
+++ server/controllers/menulist.js
@@ -2,12 +2,12 @@
 /*eslint-disable*/
 module.exports = async (ctx, next) => {
 	ctx.state.data = [
-		{ iconImg: '', iconTitle: '我的藏书', redirectUrl: '' },
-		{ iconImg: '', iconTitle: '我的关注', redirectUrl: '' },
-		{ iconImg: '', iconTitle: '我的书友', redirectUrl: '' },
-		{ iconImg: '', iconTitle: '心愿书单', redirectUrl: '' },
-		{ iconImg: '', iconTitle: '个性化设置', redirectUrl: '', hasGap: true },
-		{ iconImg: '', iconTitle: '检查更新', redirectUrl: '' },
-		{ iconImg: '', iconTitle: '关于我们', redirectUrl: '' }
+		{ ID: 1, iconImg: '', iconTitle: '我的藏书', redirectUrl: '' },
+		{ ID: 2, iconImg: '', iconTitle: '我的关注', redirectUrl: '' },
+		{ ID: 3, iconImg: '', iconTitle: '我的书友', redirectUrl: '' },
+		{ ID: 4, iconImg: '', iconTitle: '心愿书单', redirectUrl: '' },
+		{ ID: 5, iconImg: '', iconTitle: '个性化设置', redirectUrl: '', hasGap: true },
+		{ ID: 6, iconImg: '', iconTitle: '检查更新', redirectUrl: '' },
+		{ ID: 7, iconImg: '', iconTitle: '关于我们', redirectUrl: '' }
 	]
 }
