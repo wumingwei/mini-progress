a936e72963223fa6698c900371a3b0e594a97b7b
wumingwei <wu_mingwei@yeah.net>
2018-09-08 16:53:56 +0800

    modify  book module's name  to  be classify

--- src/pages/book/main.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-import Book from './book.vue'
-
-const app = new Vue(Book)
-app.$mount()
---
+++ src/pages/classify/classify.vue
@@ -0,0 +1,26 @@
+
+<template>
+	<div class='page-classify'>
+		分类搜索模块
+	</div>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		ready() {
+
+		},
+		attached() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+<style lang='scss' scoped>
+</style>
