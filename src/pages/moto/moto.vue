bf83f7cb3caac5acbe76bdceadd0857cad0a06fa
wumingwei <wu_mingwei@yeah.net>
2018-11-25 00:07:16 +0800

    add showModel

--- src/pages/moto/moto.vue
+++ src/pages/moto/moto.vue
@@ -12,60 +12,64 @@
     </div>
 </template>
 <script>
-    /* eslint-disable */
-    import { get, showToast, showModel } from '@/utils';
-    import config from '@/config';
-    export default {
-        data() {
-            return {
-                motoList: [],
-            };
-        },
-        async created() {
-            // 请求moto数据
-            const motoList = await get('/weapp/motolist');
-            console.log('motoListData:', motoList);
-            this.motoList = motoList;
+/* eslint-disable */
+import { get, showToast, showModel } from '@/utils';
+import config from '@/config';
+export default {
+    data() {
+        return {
+            motoList: [],
+        };
+    },
+    async created() {
+        // 请求moto数据
+        showToast('loading', '请求数据中...');
+        const motoList = await get('/weapp/motolist');
+        showToast('success', '请求成功');
+        console.log('motoListData:', motoList);
+        this.motoList = motoList;
 
-            // 豆瓣书单的数据加载
-        },
-        // 下拉刷新
-        onPullDownRefresh() {
-            wx.startPullDownRefresh();
-            wx.stopPullDownRefresh();
-        },
+        // 豆瓣书单的数据加载
+    },
+    // 下拉刷新
+    onPullDownRefresh() {
+        wx.startPullDownRefresh();
+        wx.stopPullDownRefresh();
+    },
 
-        methods: {},
-    };
+    methods: {},
+};
 </script>
+
+
 <style lang="scss" scoped>
-    .page-moto {
-        width: 100%;
-        height: 100%;
-        background: #f6f6f6;
-        // moto座右铭
-        .index-moto {
-            width: 80%;
-            padding: 5%;
-            margin: 20rpx auto;
-            background-color: #fff;
-            box-shadow: 10rpx 10rpx 6rpx #e5e5e5;
-            &__content {
-                font-size: 14px;
-                color: #666;
-                margin-bottom: 10rpx;
-                width: 100%;
-                line-height: 50rpx;
-                text-indent: 56rpx;
-            }
-            &__author {
-                width: 100%;
-                height: 40rpx;
-                text-align: right;
-                line-height: 50rpx;
-                font-size: 12px;
-                color: #ccc;
-            }
+.page-moto {
+    width: 100%;
+    height: 100%;
+    background: #f6f6f6;
+    // moto座右铭
+    .index-moto {
+        width: 80%;
+        padding: 5%;
+        margin: 20rpx auto;
+        background-color: #fff;
+        box-shadow: 10rpx 10rpx 6rpx #e5e5e5;
+        &__content {
+            font-size: 14px;
+            color: #666;
+            margin-bottom: 10rpx;
+            width: 100%;
+            line-height: 50rpx;
+            text-indent: 56rpx;
+        }
+        &__author {
+            width: 100%;
+            height: 40rpx;
+            text-align: right;
+            line-height: 50rpx;
+            font-size: 12px;
+            color: #ccc;
         }
     }
+}
 </style>
