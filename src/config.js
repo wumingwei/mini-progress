d026b231b488aa6b0c9c3456223d4824e09e0cd0
wumingwei <wu_mingwei@yeah.net>
2018-11-25 00:13:53 +0800

    add  comment

--- src/config.js
+++ src/config.js
@@ -1,6 +1,13 @@
-// 配置项
+// 系统配置项
 const host = 'http://localhost:5757';
 
+/**
+ * 数据请求 url 配置项
+ * @param {String}  host 数据请求 baseUrl
+ * @param {String}  loginUrl 登录url
+ * @param {String}  userUrl 获取登录人信息url
+ * @param {String}  demoUrl 接口服务测试 url
+ */
 const config = {
     host,
     loginUrl: `${host}/weapp/login`,
--- src/utils.js
+++ src/utils.js
@@ -1,45 +1,115 @@
 // 工具函数库
+// 引入全局配置项
 import config from './config';
 
-// http get请求的二次封装
-export function get (url) {
+// http get post请求的二次封装
+/**
+ * get 数据请求
+ *
+ * @param {String} url 请求 url
+ * @param {Object} data 请求参数
+ * @returns
+ */
+export function get (url, data) {
+    return request(url, 'GET', data);
+}
+
+
+/**
+ * post 数据请求
+ *
+ * @param {String} url 请求 url
+ * @param {Object} data 请求参数
+ * @returns
+ */
+export function post (url, data) {
+    return request(url, 'POST', data);
+}
+
+/**
+ * wx.request 数据请求
+ *
+ * @param {String} url 请求的 url
+ * @param {String} method 请求方法
+ * @param {Object} data  请求参数
+ * @param {Object} header 请求头
+ * @returns
+ */
+function request (url, method, data, header = {}) {
     return new Promise((resolve, reject) => {
         wx.request({
+            data,
+            method,
+            header,
             url: config.host + url,
             success: function (res) {
                 if (res.data.code === 0) {
                     resolve(res.data.data);
                 } else {
+                    showModel({
+                        title: '提示',
+                        content: res.data.msg
+                    });
                     reject(res.data);
                 }
             },
-            fail: function (res) {
-                console.error('fail:', res);
-            },
+            // fail: function (res) {
+            //     console.error('fail:', res);
+            // },
         });
-    });
+    })
 }
 
+/**
+ * Toast 弹窗
+ *
+ * @param {String} type
+ * @param {String} text
+ * @param { Number} duration
+ * @param {Boolean} ifHasMask
+ */
 export function showToast (type, text, duration, ifHasMask) {
     wx.showToast({
-        title: text,
         icon: type, // success/loading/none
+        title: text,
         duration: duration || 1500,
         mask: ifHasMask || false,
     });
 }
 
-export function showModel (params) {
+/**
+ * Model 弹窗
+ *
+ * @param {String} title 标题
+ * @param {String} content 弹窗内容
+ * @param {Object} cancleConfig 取消按钮配置项
+ * @param {Object} confirmConfig 确认按钮配置项
+ * @param {Function} successCallback 确认回调事件
+ * @param {Function} failCallback 取消回到事件
+ * @param {Function} completeCallback 完成回调事件
+ */
+export function showModel (
+    title,
+    content,
+    cancleConfig,
+    confirmConfig,
+    successCallback,
+    failCallback,
+    completeCallback
+) {
     wx.showModal({
-        title: params.title || '提示',
-        content: params.content,
-        showCancel: params.content || false,
-        cancelText: params.cancelText || '取消',
-        cancelColor: params.cancelColor || '#000000',
-        confirmText: params.confirmText || '确定',
-        confirmColor: params.confirmColor || '#3CC51F',
-        success: params.success || function () {},
-        fail: params.fail || function () {},
-        complete: params.complete || function () {},
+        title: title || '提示',
+        content: content,
+
+        showCancel: cancleConfig.showCancle || false,
+        cancelText: cancleConfig.cancelText || '取消',// 取消按钮的文字，最多 4 个字符
+        cancelColor: cancleConfig.cancelColor || '#000000',
+
+        confirmText: confirmConfig.confirmText || '确定',// 确定按钮的文字，最多 4 个字符
+        confirmColor: confirmConfig.confirmColor || '#3CC51F',
+
+        success: successCallback || function () {},
+        fail: failCallback || function () {},
+        complete: completeCallback || function () {},
     });
 }
